07480dde063be1422f7b513560891c0df95ff5bb
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-28 18:22:09 +0200

    refactor function name to BirthdayCardPreview()

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     HappyBirthdayTheme {
         Greeting("Android")
     }
