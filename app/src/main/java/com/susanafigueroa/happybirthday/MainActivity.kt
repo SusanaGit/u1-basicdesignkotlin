267633b4df5f1dd8c3c0786d08f3fa8ad90d693c
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-28 18:22:54 +0200

    change Greeting's name

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        Greeting("Android")
+        Greeting("Susana")
     }
 }
