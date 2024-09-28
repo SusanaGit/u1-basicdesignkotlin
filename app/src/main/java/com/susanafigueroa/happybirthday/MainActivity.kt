52a9540ee861eb962d332d7760518d271ea06b0e
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-28 18:47:24 +0200

    using column to organize text elements

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -33,15 +34,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column {
+        Text(
+            text = message,
+            fontSize = 50.sp,
+            lineHeight = 116.sp
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
