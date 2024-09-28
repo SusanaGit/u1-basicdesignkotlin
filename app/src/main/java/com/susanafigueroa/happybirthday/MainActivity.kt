a10860574dab8e5af7bfc1945b02048e1a505d7b
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-29 00:13:39 +0200

    add more string resources

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     GreetingImage(
                         message = stringResource(R.string.happy_birthday_susana),
-                        from = "From Alex",
+                        from = stringResource(R.string.from_alex),
                         modifier = Modifier.padding(8.dp)
                     )
                 }
@@ -93,6 +93,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Susana!", from = "From Alex")
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_susana),
+            from = stringResource(R.string.from_alex)
+        )
     }
 }
