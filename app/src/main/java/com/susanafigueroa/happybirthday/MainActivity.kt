698f65dea65dfce076500bbef999fd962c36735e
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-28 18:53:10 +0200

    align center 'Happy Birthday Susana!'

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.susanafigueroa.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -26,6 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
                 }
             }
         }
@@ -34,11 +39,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ){
         Text(
             text = message,
             fontSize = 50.sp,
-            lineHeight = 116.sp
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
         )
         Text(
             text = from,
