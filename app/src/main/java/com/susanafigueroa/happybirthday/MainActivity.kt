bf62983b47ca37b437e66334e47eee41ec6f2736
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-29 00:01:05 +0200

    change the opacity of background

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -75,7 +75,8 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
         Image(
             painter = image,
             contentDescription = null,
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
         GreetingText(
             message = message,
