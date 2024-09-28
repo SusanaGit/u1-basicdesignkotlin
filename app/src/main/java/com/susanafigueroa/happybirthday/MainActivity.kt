13803b2dfe5c6911758da15a3b24b693ca3c5fda
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-29 00:07:41 +0200

    extract resource

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     GreetingImage(
-                        message = "Happy Birthday Susana!",
+                        message = stringResource(R.string.happy_birthday_susana),
                         from = "From Alex",
                         modifier = Modifier.padding(8.dp)
                     )
