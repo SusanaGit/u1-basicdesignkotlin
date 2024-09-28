7eaa502afefb25a5cb8a53d6e87549c320a7a6a9
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-28 18:58:51 +0200

    second text alignment end

--- app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
+++ app/src/main/java/com/susanafigueroa/happybirthday/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,7 +52,10 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
         )
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 25.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
@@ -60,6 +64,6 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday Susana!",  from = "From Emma")
+        GreetingText("Happy Birthday Susana!",  from = "From Alex")
     }
 }
